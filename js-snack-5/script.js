1319ca3a562c55e92a71a0d77322703d6aaadcba
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-24 16:41:06 +0200

    js snack 5 complete

---
+++ js-snack-5/script.js
@@ -0,0 +1,41 @@
+
+// Scrivi una funzione che accetti tre argomenti:
+// un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo
+// quanto il numero di elementi dell’array).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione
+// compresa tra “a” e “b”
+
+const animals = [
+    "Aardvark",
+    "Albatross",
+    "Alligator",
+    "Alpaca",
+    "Ant",
+    "Anteater",
+    "Antelope",
+    "Ape",
+    "Armadillo",
+    "Donkey",
+    "Baboon",
+    "Badger",
+    "Barracuda"
+];
+
+const inBetween = (list, firstNumber, secondNumber) => {
+
+    const newArray = [];
+
+    list.forEach((element, i) => {
+        
+        if ( i >= firstNumber - 1 && secondNumber < list.length) {
+
+            newArray.push(element);
+        }
+    })
+
+    return newArray;
+}
+
+const newArray = inBetween(animals, 4, 11);
+
+console.log(newArray);
